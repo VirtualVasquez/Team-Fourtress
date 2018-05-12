bbdb4e5b9c5363a299bb58060582598014a18497
MEGA-MAN-EXE\wolfe <mxv647@med.miami.edu>
2018-05-12 13:52:25 -0400

    Basic Framework established, no NPMs installed

---
+++ controllers/methodController.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var router = express.Router();
+
+//Import the model (TBA) to use its database functions
+
+	//router.get "method of the day"
+	//router.get "next method"
+	//router.get "user comments"
+
+	//router.post "search route"
+	//router.post "single like"
+	//router.post "single dislike"
+
+	//router.put "create user comment"
+	//router.put "create reply to comment"
+	//router.put "all user comments"
+	//router.put "all user replies"
+
+	//router.delete "delete comment"
+	//router.delete "delete reply"
+
---
+++ models/method.js
@@ -0,0 +1,1 @@
+//Mario wants to create his own to better coordinate with his data scraper. This will be left untouched for now
