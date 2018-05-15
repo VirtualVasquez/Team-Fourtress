a6c43b2df88666bc9ea56c4db2cf00d149c0a564
MEGA-MAN-EXE\wolfe <mxv647@med.miami.edu>
2018-05-14 21:12:29 -0400

    minor ORM updates

--- config/orm.js
+++ config/orm.js
@@ -1,14 +1,43 @@
-//Mario is going to attempt writing his own ORM
-
 //Import MySQL connection
 var connection = requiire ("../config/connection.js");
-
+//================================================================================
 //insert helper function for SQL syntax from CatsApp if failure to configure ORM
+// function printQuestionMarks(num) {
+//   var arr = [];
 
-//Helper function to vonert object key/value pairs to SQL syntax from CatsApp if necessary
+//   for (var i = 0; i < num; i++) {
+//     arr.push("?");
+//   }
 
+//   return arr.toString();
+// }
+
+// // Helper function to convert object key/value pairs to SQL syntax
+// function objToSql(ob) {
+//   var arr = [];
+
+//   // loop through the keys and push the key/value as a string int arr
+//   for (var key in ob) {
+//     var value = ob[key];
+//     // check to skip hidden properties
+//     if (Object.hasOwnProperty.call(ob, key)) {
+//       // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+//       if (typeof value === "string" && value.indexOf(" ") >= 0) {
+//         value = "'" + value + "'";
+//       }
+//       // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+//       // e.g. {sleepy: true} => ["sleepy=true"]
+//       arr.push(key + "=" + value);
+//     }
+//   }
+
+//   // translate array of strings to a single comma-separated string
+//   return arr.toString();
+// }
+//================================================================================
 //Object for all our SQL statement functions. Can apply to each table
 var orm = {
+	//can be used for methods, comments, and replies as necessary
 	all: function(tableInput, cb) {
 		var queryString = "SELECT * FROM " + tableInput + ";";
 		connection.query(queryString, function(err, result) {
@@ -18,7 +47,8 @@ var orm = {
 			cb(result);
 		})
 	},
-	randomOne: function(table, cols, vals, cb){
+	//readOne can be used as "get Random" if `vals` is randomly selected
+	readOne: function(table, cols, vals, cb){
 		var queryString = "SELECT FROM " + table;
 
 		queryString += " (";
@@ -37,14 +67,29 @@ var orm = {
 			cb(result);
 		});
 	},
-	readOne: function(){
+	//this should end up getting used for comments, replies, and new users
+	create: function(table, cols, vals, cb){
+		var queryString = "INSERT INTO " + table;
+
+    	queryString += " (";
+    	queryString += cols.toString();
+    	queryString += ") ";
+    	queryString += "VALUES (";
+    	queryString += vals.length;//printQuestionMarks(vals.length)
+    	queryString += ") ";		
 
 	},
-	create: function(){
+	//used to update boolean values (like/dislike) or user comments/replies
+	update: function(table, objColVals, condition, cb) {
+		var queryString = "UPDATE " + table;
 
-	},
-	update: function(){
+		queryString += " SET ";
+		queryString += objColVals; //might need CatsApp, rewrite as ```objToSql(objColVals)```
+		queryString += " WHERE ";
+		queryString += condition;
 
+		console.log(queryString);
+		conection.query(queryString, function(err, result))
 	},
 	delete: function(){
 
