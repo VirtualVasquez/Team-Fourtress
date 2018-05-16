d1d56f5e1dbb13261c83677277da9feaab72b419
Mario Ricart <mario.r69@gmail.com>
2018-05-16 17:15:04 -0400

    added to orm

--- config/orm.js
+++ config/orm.js
@@ -1,147 +1,173 @@
 //Import MySQL connection
 var connection = require("../config/connection.js");
-//================================================================================
-//insert helper function for SQL syntax from CatsApp if failure to configure ORM
-// function printQuestionMarks(num) {
-//   var arr = [];
 
-//   for (var i = 0; i < num; i++) {
-//     arr.push("?");
-//   }
-
-//   return arr.toString();
-// }
-
-// // Helper function to convert object key/value pairs to SQL syntax
-// function objToSql(ob) {
-//   var arr = [];
-
-//   // loop through the keys and push the key/value as a string int arr
-//   for (var key in ob) {
-//     var value = ob[key];
-//     // check to skip hidden properties
-//     if (Object.hasOwnProperty.call(ob, key)) {
-//       // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
-//       if (typeof value === "string" && value.indexOf(" ") >= 0) {
-//         value = "'" + value + "'";
-//       }
-//       // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-//       // e.g. {sleepy: true} => ["sleepy=true"]
-//       arr.push(key + "=" + value);
-//     }
-//   }
-
-//   // translate array of strings to a single comma-separated string
-//   return arr.toString();
-// }
-//================================================================================
-//Object for all our SQL statement functions. Can apply to each table
 var orm = {
 
 	//countEntries
 	//counts how many entries in the table provided
 	countEntries: (table)=>{
 		var queryString = "SELECT COUNT(*) as 'count' FROM ??;";
-		connection.query(queryString, table, function(err, result) {
-			if (err) {
-				console.log(err);
-			}
-			console.log(result);
-		})
+		
+		return new Promise ((resolve,reject)=>{
+			connection.query(queryString, table, function(err, result) {
+				if (err) {
+					reject(err);
+				}
+				resolve(result);
+			});
+		});
 	},
 
 	//give this the number of entries in our table
+	//and it will select and return a random number between 1 and that
+	//this should not return a promise
 	selectRandom: (rows)=>{
 		return Math.floor(Math.random() * (rows+1));
 	},
 
-	// //select and retur
-	// all: function(tableInput, cb) {
-	// 	var queryString = "SELECT * FROM " + tableInput + ";";
-	// 	connection.query(queryString, function(err, result) {
-	// 		if (err) {
-	// 			throw err;
-	// 		}
-	// 		cb(result);
-	// 	})
-	// },
-	// //readOne can be used as "get Random" if `vals` is randomly selected
-	// readOne: function(table, cols, vals, cb){
-	// 	var queryString = "SELECT FROM " + table;
+	//give this an id
+	//it will return an object
+	//test will look for returnedObject.id === idGiven
+	//this should return a promise
+	selectMethodById: (methodId)=>{
 
-	// 	queryString += " (";
-	// 	queryString += cols.toString();
-	// 	queryString += ") ";
-	// 	queryString += "VALUES (";
-	// 	queryString += vals.length;
-	// 	queryString += ") ";
+	},
 
-	// 	console.log(queryString);
+	//selects all methods in the db
+	//in an array of objects
+	//test will check that array has same amount of elements in the db
+	// or result.length === countEntries('methods');
+	//this should return a promise
+	selectAll: ()=>{
 
-	// 	connection.query(queryString, vals, function(err, result) {
-	// 		if (err) {
-	// 			throw.err;
-	// 		}
-	// 		cb(result);
-	// 	});
-	// },
-	// //this should end up getting used for comments, replies, and new users
-	// create: function(table, cols, vals, cb){
-	// 	var queryString = "INSERT INTO " + table;
+	},
 
- //    	queryString += " (";
- //    	queryString += cols.toString();
- //    	queryString += ") ";
- //    	queryString += "VALUES (";
- //    	queryString += vals.length;//printQuestionMarks(vals.length)
- //    	queryString += ") ";
+	//give this the name of a category and it will
+	//return an array of objects with all methods under one category
+	//test will check for result[0].category === cat(argument)
+	//this should return a promise resolves with the full result.
+	categorySelect: cat=>{
 
- //    	console.log(queryString);
+	},
 
- //    	connection.query(queryString, vals, function(err, result) {
- //    		if (err) {
- //    			throw err;
- //    		}
+	//this will select a method by its name, that belongs to a category
+	//this returns a string of objects.
+	//test will check that result[0].name === name(argument);
+	//this should return a promise, that resolves with the full result.
+	selectMethodByNameInCategory: (cat,name)=>{
 
- //    		cb(result);
- //    	});		
-	// },
+	},
 
-	// //used to update boolean values (like/dislike) or user comments/replies
-	// //`condition` might not be the right term/argument to use
-	// update: function(table, objColVals, statement, cb) {
-	// 	var queryString = "UPDATE " + table;
 
-	// 	queryString += " SET ";
-	// 	queryString += objColVals; //might need CatsApp, rewrite as ```objToSql(objColVals)```
-	// 	queryString += " WHERE ";
-	// 	queryString += statement;
+	//section below should be finetuned once auth complete
 
-	// 	console.log(queryString);
-	// 	conection.query(queryString, function(err, result){
-	// 		if (err) {
-	// 			throw err;
-	// 		}
+	//this gets the value of likes in db for current method
+	//test will check result is a number
+	getLikesForMethod: methodId=>{
 
-	// 		cb(result);
-	// 	});
-	// },
-	// //delete a comment or reply, being passed through as the argument `statement`
-	// //probably a better way to write that?
-	// delete: function(table, statement, cb){
-	// 	var queryString = "DELETE FROM " +  table;
-	// 	queryString += " WHERE ";
-	// 	queryString += statement;
+	},
 
-	// 	connection.query(queryString, function (err, result) {
-	// 		if (err) {
-	// 			throw err;
-	// 		}
+	//this gets the dislikes for the current method
+	//test will check result is a number, and if given an invalid id, like 45678, returns false
+	getDislikesForMethod: methodId=>{
 
-	// 		cb(result);
-	// 	});
+	},
 
-	// },
+	//adds one to the current value of likes for this method, in the db
+	//this should return a promise
+	//test for result is number, false if invalid id
+	postLikeForMethod: methodId=>{
+
+	},
+
+
+	//subtracts one from value of likes
+	///returns a promise resolves on the result
+	// test for result is number, false if invalid id given
+	removeLikeForMethod: methodId=>{
+
+	},
+
+	//adds one to  value of dislikes
+	///returns a promise resolves on the result
+	// test for result is number, false if invalid id given
+	postDislikeForMethod: methodId=>{
+
+	},
+
+	//subtracts one from dislikes
+	///returns a promise resolves on the result
+	// test for result is number, false if invalid id given
+	removeDislikeForMethod: methodId=>{
+
+	},
+
+	//***ONCE AUTH COMPLETE--- updatemethods liked/disliked array//
+
+
+	//get all comments for the method id given
+	///returns a promise resolves on the result
+	// test for result is an array
+	getCommentsForMethod: methodId=>{
+
+	},
+
+	//gets all repplies for a comment Id num
+	///returns a promise resolves on the result
+	// test for result is an array
+	getRepliesForComment: commentId=>{
+
+	},
+
+	//adds a new comment to db, attached to the method id given
+	/////returns a promise resolved on query result
+	// test for new value of methodId is 1 greater than before
+	postCommentOnMethod: methodId=>{
+
+	},
+
+	//once auth complete: remove comment function.
+
+	postReplyOnComment: (commentId,message)=>{
+
+	},
+
+	//once auth complete
+
+	getLikesForComment: commentId=>{
+
+	},
+
+	getDislikesForComment: commentId=>{
+
+	},
+
+	postLikeForComment: commentId=>{
+
+	},
+
+	postDislikeForComment: commentId=>{
+
+	},
+
+	removeLikeForComment: commentId=>{
+
+	},
+
+	removeDislikeForComment: commentId=>{
+		
+	},
+
+	//definitely after auth
+	// getAllCommentsByUser: userId=>{
+
+	// }
+
+
+
+
+
+	
 };
 
 
