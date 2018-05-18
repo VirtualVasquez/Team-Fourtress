ef815e6b54e6d408baa9742ac2f4168c9ded5121
MEGA-MAN-EXE\wolfe <mxv647@med.miami.edu>
2018-05-17 20:12:43 -0400

    good progress through functional testing

--- test/test.js
+++ test/test.js
@@ -1,9 +1,40 @@
-var expect = require("chai").expect;
+var request = require("supertest");
 
-describe('CountEntries', function() {
-  it('should list ALL blobs on /blobs GET');
-  it('should list a SINGLE blob on /blob/<id> GET');
-  it('should add a SINGLE blob on /blobs POST');
-  it('should update a SINGLE blob on /blob/<id> PUT');
-  it('should delete a SINGLE blob on /blob/<id> DELETE');
+describe("Method Routes", function() {
+
+    it("Responds to '/'", function(done) {
+        request(server).get("/").expect(200, done);
+    });	
+	it("Get All Methods", function(done) {
+        request(server).get("/PSEUDOROUTE")
+        .set("Accept", "application/json")
+        .expect(200, done) // code for this Test SHOULD stop here.
+        .expect(PSEUDO2);
+        //pseudoroute = "/api/methods/"?
+        //PSEUDO2 = if it gets all methods, we'd have to hard code all 365 of them.
+    });
+    it("Count Entries of table", function(done) {
+        request(server).get("/PSEUDOROUTE")
+        .set("Accept", "application/json")
+        .expect(200)
+        .expect("PSUEDO2");
+        //PSEUDO2 = what was it, ```{"rows": "365"}```?
+    });
+    it("Get Method By the first ID", function(done) {
+    	//I NEED to see the information in the actual database to ensure
+    	//I put in the right info
+        request(server).get("/api/methods/1")
+        .set("Accept", "application/json")
+        .expect(200)
+        .expect({"name":"", "url":"", "category":"",
+         "description":"", "description2":"", "syntax":"",
+          "parameters":"", "returns":"", "examples":"", "tags":"",
+          "likes":"","dislikes":"", "id":"",}, done);
+    });
+    it("Adds a new author and responds with the json for the new author", function(done) {
+        request(server).post("/PSEUDOROUTE", {name:"Yamil"})
+        .set("Accept", "application/json")
+        .expect(200)
+        .expect({ name: 'Yamil' }, done);
+    });
 });
