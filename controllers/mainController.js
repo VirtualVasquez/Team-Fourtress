fe69dc708fd854e1e797a5b87d36ef1f5ac34721
Mario Ricart <mario.r69@gmail.com>
2018-05-18 17:40:47 -0400

    r

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -3,10 +3,8 @@ var router = express.Router();
 
 const db = require('../models')
 
+	//get a random
 	router.get('/', (req,res)=>{
-		// results = db.Method.findAll({
-  // 			attributes: [[sequelize.fn('COUNT')]]
-		// });
 
 		db.method.findAll().then(result=>{
 			//select a random one from the result and send
@@ -17,7 +15,20 @@ const db = require('../models')
 			if(e) throw e;
 		});
 
-	})
+	});
+
+	router.get('/search/:cat/:name', (req,res)=>{
+
+		db.method.findAll().then(result=>{
+			//select a random one from the result and send
+			console.log(result[Math.floor((Math.random() * (result.length + 1)))]);
+			res.render('method', result[Math.floor((Math.random() * (result.length + 1)))]);
+		})
+		.catch(e=>{
+			if(e) throw e;
+		});
+
+	});
 
 //API
 //==============================================
